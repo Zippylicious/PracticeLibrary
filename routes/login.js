b4926f8bab7dd09b2220200505660bd9a0ce5700
Zippylicious <zakthompson97@gmail.com>
2017-12-25 00:06:28 -0600

    Actual working basic login

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,11 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+const passHash = require('password-hash');
+const emailValid = require("email-validator");
+const connection = mysql.createConnection({
 	host	: 'localhost',
 	user	: 'root',
 	password: 'password',
-	database: 'users'
+	database: 'practice_library'
 });
 connection.connect(function(err){
 	if(err){
@@ -14,9 +16,92 @@ connection.connect(function(err){
 });
 
 exports.register = function(req, res){
+	var today = new Date();
 
+	if(!emailValid.validate(req.body.email)){
+		return res.render('register', {message: "This email is not valid, please try again."});
+	}
+
+	if(req.body.password == null || req.body.password.length < 8){
+		return res.render('register', {message: "This password is not valid, please try again.\nRemember passwords must be longer than 7 characters."});
+	}
+
+	var user = {
+		"first_name": req.body.first_name,
+		"last_name": req.body.last_name,
+		"username": req.body.username,
+		"email": req.body.email,
+		"password": passHash.generate(req.body.password),
+		"created": today,
+		"modified": today
+	}
+
+	if(!checkProp(user)){
+		return res.render('register', {message: "Not all fields were filled out, please try again."});
+	}
+
+	//check for duplicate emails and usernames
+	console.log("USername");
+	connection.query("SELECT * FROM users WHERE username = ?", [user.username], function(err, results){
+		if(err){
+			console.log("Error checking for username duplicates: " + err);
+			return res.render('register', {message: "Sorry, an error occurred. Please try again later."});
+		} else {
+			if(results.length != 0){
+				return res.render('register', {message: "Please try another username."});
+			}
+		}
+
+			console.log("Email");
+			connection.query("SELECT * FROM users WHERE email = ?", [user.email], function(err, results){
+				if(err){
+					console.log("Error checking for username duplicates: " + err);
+					return res.render('register', {message: "Sorry, an error occurred. Please try again later."});
+				} else {
+					if(results.length != 0){
+						return res.render('register', {message: "Please try another email."});
+					}
+				}
+
+					console.log("Inster");
+					connection.query("INSERT INTO users SET ?", user, function(err, results){
+						if(err){
+							console.log("Error inserting into users" + err);
+							return res.render('register', {message: "Sorry, an error occurred. Please try again later."});
+						} else {
+							return res.render('login', {message: "You have been successfully registered! Please log in here."});
+						}
+					});
+			});
+	});
+};
+
+function checkProp(obj){
+	for(var prop in obj){
+		if(obj[prop] == null || obj[prop] == ""){
+			return false;
+		}
+	}
+	return true;
 };
 
 exports.login = function(req, res){
-
+	var username = req.body.username;
+	connection.query("SELECT * FROM users WHERE username = ?", [username], function(err, results){
+		if(err){
+			console.log("Error retrieving user from DB:" + err);
+			return res.render('login', {message: "Login failed, please try again."});
+		} else {
+			if(results.length != 0){
+				var password = results[0].password;
+				if(passHash.verify(req.body.password, password)){
+					return res.render('index'); //add in message and session stuff here
+				} else {
+					return res.render('login', {message: "Your username and password did not match, please try again."});
+				}
+			} else {
+				return res.render('login', {message: "Your username and password did not match, please try again."});
+			}
+		}
+	});
 };
--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express()
+const app = express();
+const mysql = require('mysql');
 
-const login = require('.routes/loginroutes');
+const login = require('./routes/login');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,18 +11,21 @@ app.use(bodyParser.json());
 app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index', {input:null, error:'Nothing entered yet'});
-})
+   res.render('login', {message: null});
+});
 
-app.post('/', function (req, res) {
-  var input = req.body.input_field;
-  if(input){
-  	res.render('index', {input:input, error:null})
-  } else{
-  	res.render('index', {input:null, error:'Nothing found'});
-  }  
-})
+app.get('/login', function (req, res) {
+  	res.render('login', {message: null});
+});
+
+app.get('/register', function(req, res) {
+	res.render('register', {message: null})
+});
+
+app.post('/register', login.register);
+
+app.post('/login', login.login);
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+   console.log('Example app listening on port 3000!')
+});
